8617098b06dafd5267b72e707d4e18df0eea22b5
Gabriel Migliatti <101683285+Migliatti@users.noreply.github.com>
2023-04-10 19:50:39 -0300

    tentando arrumar o site dnv

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import PrincipalPage from "./pages/PrincipalPage";
+import Form from "./components/Form";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <PrincipalPage />
+    <Form />
   </React.StrictMode>
 );
